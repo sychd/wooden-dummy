d6b57e1a047e7b503c9de25de3488d7b1d1b2fce
sychd <denis.feelin@gmail.com>
2018-03-18 14:11:29 +0200

    functor desc + example added

--- fp/functor.js
+++ fp/functor.js
@@ -1,36 +1,47 @@
-// Code sample & text were taken from 'Functional Programming in JavaScript' by Luis Atencio, ch. 5
-
-const R = require('ramda');
-
-class Wrapper {
+// ALERT: I'm not shure that it is 100% correct description :)
+//
+// tl;dr - Functor is that you can map().
+//
+// So, what is functor?
+// Functor = fmap(mapFn) + context
+// fmap - function, that applies mapFn to context's value.
+// Context - 'surrounding object'. I.e. minimal context is object with 'value' property.
+// Actually, Context is a Functor (sorry for confusing, but nothing can do).
+// Functor is any data type if it "knows how to fmap()".
+class Context {
     constructor(value) {
-        this._value = value;
+        this.value = value;
     }
-    // map :: (A -> B) -> A -> B
-    map(f) {
-        return f(this._value);
-    };
 
-    toString() {
-        return 'Wrapper (' + this._value + ')';
+    fmap(mapFn) {
+        const transformedValue = mapFn(this.value);
+
+        return new Context(transformedValue);
     }
 }
-// wrap :: A -> Wrapper(A)
-const wrap = (val) => new Wrapper(val);
+//
+// What does functor do?
+// 1. Unpacks value from context -> 
+// 2. Applies mapFn to context's value -> 
+// 3. Wraps result to new context (of the same type)
+//
+// Functor laws (wiki.haskell.org):
+// 1.When performing the mapping operation, if the values in the functor are mapped to 
+// themselves, the result will be an unmodified functor.
+// Or: fmap(v => v) ---> new context's value === previous context's value (as it was before fmap)
+//
+// 2.If two sequential mapping operations are performed one after the other using two functions, the result 
+// should be the same as a single mapping operation with one function that is equivalent to applying the first
+// function to the result of the second.
+// Or: fmap(a(b('lol'))) === fmap(a(fmap(b('lol')))) - it is like function composition
+const functor = new Context('Hi there');
+const transformed = functor
+        .fmap(v => v.toUpperCase())
+        .fmap(v => v.concat('!'));
+        
+console.log(transformed); // Context { value: 'HI THERE!' }
 
-const wrappedValue = wrap('Get Functional');
-wrappedValue.map(R.identity); //-> 'Get Functional'
-
-// fmap knows how to apply functions to values wrapped in a context. It first opens the
-// container, then applies the given function to its value, and finally closes the value back
-// into a new container of the same type. This type of function is known as a functor.
-
-// fmap :: (A -> B) -> Wrapper[A] -> Wrapper[B]
-Wrapper.prototype.fmap = function fmap(f) {
-    const transformedValue = f(this._value);
-    
-    return new Wrapper(transformedValue);
-};
-
-const wrapped = wrap(1337);
-console.log(wrapped.fmap(R.inc)); // Wrapper { _value: 1338 }
+// So, cool. But what's the deal? Where is the profit?
+// In 'Context' class there is no actual profit. The context can be anything (and useful).
+// Actually, it goes to monads usage where context (=== specific monad) gives you different
+// perks like null-proof code (Maybe/Either monads) and other stuff that should be covered futrher.
