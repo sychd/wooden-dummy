7160d2ee538b886b35f225a008d2c13022973738
dsych <denis.feelin@gmail.com>
2019-09-13 17:13:02 +0300

    deps updated, ts, ts-fp added

---
+++ fp/fp-ts/index.ts
@@ -0,0 +1,4 @@
+import {IO} from "fp-ts/lib/IO";
+const log = (s: unknown): IO<void> => () => console.log(s);
+
+log('Hello world!')();
--- rxjs/concat-all.operator.example.js
+++ rxjs/concat-all.operator.example.js
@@ -1,13 +1,18 @@
-import * as Rx from "rxjs";
+const { map, concatAll } = require("rxjs/operators");
+const { range } = require("rxjs");
 
-var source = Rx.Observable.range(0, 3)
-    .map(function (x) {
-        const ob = `Ob${x}`;
-        return Rx.Observable.range(x, 3).map(x => `${ob} ${x}`);
-    })
-    .concatAll(); //same with mergeAll(), but concatAll() keeps timeline
+const source = range(0, 3).pipe(
+    map(x => {
+            const ob = `Ob${x}`;
+            return range(x, 3).pipe(
+                map(x => `${ob} ${x}`)
+            );
+        }
+    ),
+    concatAll()//same with mergeAll(), but concatAll() keeps timeline
+);
 
-var subscription = source.subscribe(
+source.subscribe(
     function (x) {
         console.log('Next: %s', x);
     },
